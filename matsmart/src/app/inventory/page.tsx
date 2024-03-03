97c046dbb08315f67645e4ab0fa2d15e33673938
Oscar <oscarbe@stud.ntnu.no>
2024-03-03 18:05:37 +0100

    Lagt til databse "proof of consept"
    Lagt inn database kobling (OBS, koblingen er i plain text)
    Redigerte inventory/page.tsx til å vise navnet på alt som er i item_database tabellen

--- matsmart/src/app/inventory/page.tsx
+++ matsmart/src/app/inventory/page.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { query } from "../lib/db"
 
-export default function Page() {
+export default async function Page() {
+//Kjører spørring mot databsen
+  const dbtest = await query({
+    query: "SELECT * FROM item_database",
+    values: [],
+  });
+
   return (
     <>
       <Link href="inventory/pantry">Pantry</Link>
@@ -10,6 +17,13 @@ export default function Page() {
       <Link href="inventory/freezer">Freezer</Link>
       <br />
       <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Add to inventory</a>
+
+    {/* Usikker på hvordan man får vekk den error-markeringen, for det fungerer som det skal */}
+      <div>
+        {dbtest.map(item => (
+          <div key={item.item_id}>{item.item_name}</div>
+        ))}
+      </div>
     </>
   );
 }
