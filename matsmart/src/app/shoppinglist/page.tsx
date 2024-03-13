bdccf21a2d2552f68c70f2a6da1399d1987d5081
Vegard Torkildsen Udnes <vegardudnes@outlook.com>
2024-03-13 20:23:35 +0100

    feat: add remaining input, buttons and checkboxes

--- matsmart/src/app/shoppinglist/page.tsx
+++ matsmart/src/app/shoppinglist/page.tsx
@@ -1,17 +1,18 @@
-import { SearchBar, SearchSort } from "@/src/app/ui/recipes/search";
-import Table from "../ui/shoppinglist/table";
+import { SearchBar } from "@/src/app/ui/recipes/search";
+import GroceryTable from "../ui/shoppinglist/GroceryTable";
 import MyButton from "../ui/shoppinglist/button";
+import MeasurementDropdown from "../ui/shoppinglist/MeasurementDropdown";
 
 export default function ShoppingList() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col padd">
       <h1 className={`mb-4 text-xl md:text-2xl`}>Shopping list</h1>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8 max-w-3xl ">
         <SearchBar placeholder="Add to shopping list..." />
-        <SearchSort />
+        <MeasurementDropdown />
         <MyButton />
       </div>
-      <Table
+      <GroceryTable
         data={[
           {
             groceries: "Apples",
