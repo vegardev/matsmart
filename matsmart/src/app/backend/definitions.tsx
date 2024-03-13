b2a4802322b2a583d22ea0c9ec2a2e30a631e67e
Oscar <oscarbe@stud.ntnu.no>
2024-03-13 14:06:24 +0100

    Added new queries
    Added several queries to be used for fetching recipes and items form the database.

--- matsmart/src/app/backend/definitions.tsx
+++ matsmart/src/app/backend/definitions.tsx
@@ -11,3 +11,37 @@ export type Item_database = {
   item_name: string;
   item_quantity_type: string;
 };
+
+export type Recipe = {
+  recipe_id: number;
+  title: string;
+  content: string;
+  image: string;
+};
+
+export type Recipes_no_content = {
+  recipe_id: number;
+  title: string;
+  content: string;
+  image: string;
+};
+
+export type Recipe_items = {
+  item_id: number;
+  recipe_id: number;
+  item_quantity: number;
+  item_quantity_type: string;
+  item_name: string;
+};
+
+//expiration date burde nok endres type til Date
+export type Inventory_items = {
+  inventory_id: number;
+  item_id: number;
+  item_quantity: number;
+  item_quantity_type: string;
+  item_name: string;
+  item_location: string;
+  expiration_date: string;
+};
+
