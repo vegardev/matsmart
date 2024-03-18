d170d041cfcca50b66f694d97a94502284899539
Tommelskinke <eliasherigstad@gmail.com>
2024-03-18 11:59:30 +0100

    Revert "Update recipesRecommended.tsx"
    This reverts commit 11099eb33109841616fa96af606bf19ece92dca0.

--- matsmart/src/app/ui/home/recipesRecommended.tsx
+++ matsmart/src/app/ui/home/recipesRecommended.tsx
@@ -1,20 +1,11 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
-<<<<<<< HEAD
 import { Groceries } from "@/src/app/backend/definitions";
-=======
-import { Recipe } from "@/src/app/backend/definitions";
-import Link from "next/link";
->>>>>>> home
 
 export default async function RecommendedItems({
   RecommendedItems,
 }: {
-<<<<<<< HEAD
   RecommendedItems: Groceries[];
-=======
-  RecommendedItems: Recipe[];
->>>>>>> home
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
@@ -25,15 +16,13 @@ export default async function RecommendedItems({
           {RecommendedItems.map((item, i) => {
             return (
               <div
-                key={item.title}
+                key={item.item_id}
                 className={clsx("grid grid-cols-3 items-center py-4", {
                   "border-t": i !== 0,
                 })}
               >
                 <div className="min-w-0">
-                  <Link href="/shoppinglist">
-                    <p className="normal-font">{item.title}</p>
-                  </Link>
+                  <p className="normal-font">{item.item_name}</p>
                 </div>
               </div>
             );
