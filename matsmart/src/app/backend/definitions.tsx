8bb66bf3a15abc6a6557ecf4976dde0cf8c0193d
Trygve Mikalsen Kvia <trygvmk@stud.ntnu.no>
2024-03-18 20:31:04 +0100

    Update 6 files
    - /matsmart/src/app/recipes/page.tsx
    - /matsmart/src/app/backend/definitions.tsx
    - /matsmart/src/app/ui/home/sidenav.tsx
    - /matsmart/src/app/layout.tsx
    - /matsmart/next.config.mjs
    - /matsmart/tailwind.config.ts

--- matsmart/src/app/backend/definitions.tsx
+++ matsmart/src/app/backend/definitions.tsx
@@ -12,6 +12,19 @@ export type Item_database = {
   item_quantity_type: string;
 };
 
+export type Tags = {
+  tag_id: number;
+  tag_name: string;
+};
+
+export type Recipe_Preview = {
+  recipe_id: number;
+  recipe_name: string;
+  recipe_time: number;
+  recipe_image: string;
+  recipe_tags: string[];
+};
+
 export type Recipe = {
   recipe_id: number;
   title: string;
