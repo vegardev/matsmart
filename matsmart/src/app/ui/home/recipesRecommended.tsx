11099eb33109841616fa96af606bf19ece92dca0
Tommelskinke <eliasherigstad@gmail.com>
2024-03-18 11:39:11 +0100

    Update recipesRecommended.tsx

--- matsmart/src/app/ui/home/recipesRecommended.tsx
+++ matsmart/src/app/ui/home/recipesRecommended.tsx
@@ -1,11 +1,20 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
+<<<<<<< HEAD
 import { Groceries } from "@/src/app/backend/definitions";
+=======
+import { Recipe } from "@/src/app/backend/definitions";
+import Link from "next/link";
+>>>>>>> home
 
 export default async function RecommendedItems({
   RecommendedItems,
 }: {
+<<<<<<< HEAD
   RecommendedItems: Groceries[];
+=======
+  RecommendedItems: Recipe[];
+>>>>>>> home
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
@@ -16,13 +25,15 @@ export default async function RecommendedItems({
           {RecommendedItems.map((item, i) => {
             return (
               <div
-                key={item.item_id}
+                key={item.title}
                 className={clsx("grid grid-cols-3 items-center py-4", {
                   "border-t": i !== 0,
                 })}
               >
                 <div className="min-w-0">
-                  <p className="normal-font">{item.item_name}</p>
+                  <Link href="/shoppinglist">
+                    <p className="normal-font">{item.title}</p>
+                  </Link>
                 </div>
               </div>
             );
