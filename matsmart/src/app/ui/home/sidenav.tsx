d29814f19e80aaf3cb57ce13e7c26a8f5a3eb3fd
Tommelskinke <eliasherigstad@gmail.com>
2024-04-08 19:23:30 +0200

    Customer Preferences
    små justeringer

--- matsmart/src/app/ui/home/sidenav.tsx
+++ matsmart/src/app/ui/home/sidenav.tsx
@@ -6,14 +6,13 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 justify-start rounded-md bg-blue-400 p-4 md:h-40 items-center"
-        href="/home"
-      >
+        className="mb-2 flex h-20 justify-start rounded-md p-4 md:h-40 items-center"
+        href="/home">
         <Image
           src="/matsmart_logo.png"
           width={300}
           height={300}
-          alt="Matsmart logo"
+          alt="Matsmartlogo"
         />
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
