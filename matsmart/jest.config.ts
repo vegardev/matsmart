fcbfc45ff3e5e2d34c3b55ad402edb1949fb4cf3
Oscar <oscarbe@stud.ntnu.no>
2024-04-11 13:28:41 +0200

    CI enhetstest forsøk 5

--- matsmart/jest.config.ts
+++ matsmart/jest.config.ts
@@ -114,7 +114,7 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+   reporters: [ "default", "jest-junit" ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
